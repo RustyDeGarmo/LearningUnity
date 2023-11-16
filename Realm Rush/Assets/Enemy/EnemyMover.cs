45d26eb12f15de40f9590e17ee75effd7080b8be
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-11-16 15:30:42 -0600

    updated enemy mover script so the enemy moves smoothly from waypoint to waypoint instead of jumping on a timer

--- Realm Rush/Assets/Enemy/EnemyMover.cs
+++ Realm Rush/Assets/Enemy/EnemyMover.cs
@@ -5,19 +5,43 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float speed = 1f;
+    
 
-    // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(FollowPath());
+        transform.position = path[0].transform.position;
     }
 
     IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
-            gameObject.transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(1f);
+            Vector3 startPostiion = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            float rotationDegrees = degreesToRotate(this.transform.forward, startPostiion, endPosition);
+            
+            while(travelPercent < 1f)
+            {   
+                if(travelPercent < 0.25f)
+                {
+                    float rotationAngle = 4 * rotationDegrees * Time.deltaTime * speed;
+                    transform.Rotate(new Vector3(0, rotationAngle, 0));
+                }
+                
+                travelPercent += Time.deltaTime * speed;
+                transform.position = Vector3.Lerp(startPostiion, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
         }
+    }
+
+    float degreesToRotate(Vector3 currentDir, Vector3 currentPosition, Vector3 endPosition)
+    {
+        Vector3 targetDir = endPosition - currentPosition;
+        return Vector3.SignedAngle(currentDir, targetDir, Vector3.up);
     }
 }
