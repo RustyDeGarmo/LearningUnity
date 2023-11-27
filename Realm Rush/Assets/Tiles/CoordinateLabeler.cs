570b8f0e81940a87940c7c618d79589543e57035
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-11-27 13:11:49 -0600

    added methods to change the color of a label depending on whether a tower can be placed there and to toggle the labels on and off while playing

--- Realm Rush/Assets/Tiles/CoordinateLabeler.cs
+++ Realm Rush/Assets/Tiles/CoordinateLabeler.cs
@@ -6,12 +6,17 @@ using TMPro;
 [ExecuteAlways]
 public class CoordinateLabeler : MonoBehaviour
 {
+    [SerializeField] Color defaultColor = Color.white;
+    [SerializeField] Color blockedTileColor = Color.gray;
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
+    Waypoint waypoint;
 
     void Awake()
     {
         label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -21,7 +26,30 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
-        }        
+        }
+
+        ColorCoordinates();
+        ToggleLabels();
+    }
+
+    void ToggleLabels()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
+    }
+
+    void ColorCoordinates()
+    {
+        if(waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockedTileColor;
+        }
     }
 
     void DisplayCoordinates()
