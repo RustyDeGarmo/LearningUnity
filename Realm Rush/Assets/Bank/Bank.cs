dd662889b2435b8d58fa1b3c9c652731bb766afa
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2023-12-04 14:25:18 -0600

    added a label to the UI to display the current gold the player has

--- Realm Rush/Assets/Bank/Bank.cs
+++ Realm Rush/Assets/Bank/Bank.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
-
     [SerializeField] int startingBalance = 150;
+    [SerializeField] TextMeshProUGUI displayBalance;
     
     int currentBalance;
     public int CurrentBalance { get {return currentBalance;} }
@@ -14,21 +15,29 @@ public class Bank : MonoBehaviour
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateDisplay();
     }
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateDisplay();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateDisplay();
 
         if(currentBalance < 0)
         {
             //lose game
             ReloadScene();
         }
+    }
+
+    void UpdateDisplay()
+    {
+        displayBalance.text = "Gold: " + currentBalance;
     }
 
     void ReloadScene()
