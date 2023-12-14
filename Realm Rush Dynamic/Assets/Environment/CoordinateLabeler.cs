a72098987eca62fa0e81d2035dd80d09821dfaff
RustyLenovoLaptop <rustydegarmo@yahoo.com>
2023-12-14 15:04:33 -0600

    updated the coordinate labeler to work with the pathfinding system that doesn't exist yet

--- Realm Rush Dynamic/Assets/Environment/CoordinateLabeler.cs
+++ Realm Rush Dynamic/Assets/Environment/CoordinateLabeler.cs
@@ -9,16 +9,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedTileColor = Color.gray;
+    [SerializeField] Color exploredColor = Color.yellow;
+    [SerializeField] Color pathColor = new Color(1f, .5f, 0f);
+
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -45,14 +48,28 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetLabelColor()
     {
-        if(waypoint.IsPlaceable)
-        {
-            label.color = defaultColor;
-        }
-        else
+        if(gridManager == null) { return; }
+
+        Node node = gridManager.GetNode(coordinates);
+        if(node == null) { return; }
+
+        if(!node.isWalkable)
         {
             label.color = blockedTileColor;
         }
+        else if(node.isPath)
+        {
+            label.color = pathColor;
+        }
+        else if(node.isExplored)
+        {
+            label.color = exploredColor;
+        }
+        else
+        {
+            label.color = defaultColor;
+        }
+
     }
 
     void DisplayCoordinates()
