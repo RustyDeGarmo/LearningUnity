5707536f581424e9645f339c83ad1a94beff695a
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2024-02-26 22:49:10 -0600

    added muzzle flash vfx

--- Zombie Runner/Assets/Scripts/Weapon.cs
+++ Zombie Runner/Assets/Scripts/Weapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 20f;
+    [SerializeField] ParticleSystem muzzleFlash;
     
     void Update()
     {
@@ -18,25 +20,34 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.collider.name);
             // TODO: add vfx
 
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
-            if(target == null) return;
+            if (target == null) return;
 
-            target.TakeDamage(damage); 
+            target.TakeDamage(damage);
             Debug.Log(target.GetHP() + " HP remaining");
-            
+
         }
         else
         {
             return;
         }
-        
-
-        
     }
 }
