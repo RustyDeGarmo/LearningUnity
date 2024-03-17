30cd18dc945abcfd10f3fba36342d3b0f294ddb3
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2024-03-17 10:50:41 -0500

    added UI for when the player dies, it's currently always on and doesn't actually reload the level for some reason

---
+++ Zombie Runner/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System;
+
+public class SceneLoader : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
