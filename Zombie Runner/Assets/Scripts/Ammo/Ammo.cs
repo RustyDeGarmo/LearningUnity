5a3daf59f51f122f10f067a271b019c9ff6e87bc
Rusty <81254685+RustyDeGarmo@users.noreply.github.com>
2024-04-09 20:53:26 -0500

    finished the pickup script and basic meshes that can be placed in the game

--- Zombie Runner/Assets/Scripts/Ammo/Ammo.cs
+++ Zombie Runner/Assets/Scripts/Ammo/Ammo.cs
@@ -23,6 +23,11 @@ public class Ammo : MonoBehaviour
         GetAmmoSlot(ammoType).ammoAmount -= 1;
     }
 
+    public void IncreaseCurrentAmmoAmount(AmmoType ammoType, int ammoAmount)
+    {
+        GetAmmoSlot(ammoType).ammoAmount += ammoAmount;
+    }
+
     private AmmoSlot GetAmmoSlot(AmmoType ammoType)
     {
         foreach(AmmoSlot slot in ammoSlots)
--- Zombie Runner/Assets/Scripts/Ammo/AmmoPickup.cs
+++ Zombie Runner/Assets/Scripts/Ammo/AmmoPickup.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class AmmoPickup : MonoBehaviour
 {
+    [SerializeField] int ammoAmount = 5;
+    [SerializeField] AmmoType ammoType = AmmoType.PistolRounds;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            Debug.Log("did the thing");
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmoAmount(ammoType, ammoAmount);
             Destroy(gameObject);
         }    
     }
